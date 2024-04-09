609a0498f3c01266d376c4dab69b34ec28a1a924
Tomoyuki Tsujimoto <tomo.tsujimoto@gmail.com>
2024-04-09 13:47:25 +0900

    spotlight

--- examples/shader/main3.js
+++ examples/shader/main3.js
@@ -6,14 +6,14 @@ import * as dat from 'dat.gui';
 
 
 let main_camera, main_scene, main_renderer, main_material, main_mesh;
-let plane_mesh, sphere_mesh, sphere_material;
+let plane_mesh, sphere_mesh, sphere_material, spotLight;
 const background_color = 0xCCCCCC
 const canvas = document.querySelector( '#webgl_canvas' );
 const gui = new dat.GUI();
 const guiOption = {
     sphereColor: '#FF0000',
     wireframe: false,
-    speed: 0.01
+    speed: 0.01,
 }
 
 const setting = {
@@ -85,23 +85,31 @@ function setupLights(){
     const ambient_light = new THREE.AmbientLight( 0x333333 );
     main_scene.add( ambient_light );
 
-    // ライト
-    const direction_light = new THREE.DirectionalLight( setting.light_color, setting.light_intensity );
-    direction_light.position.set( setting.light_x, setting.light_y, setting.light_z );
-    main_scene.add( direction_light );
-    direction_light.castShadow = true;
-    direction_light.shadow.camera.left = -20
-    direction_light.shadow.camera.right = 10
-    direction_light.shadow.camera.top = 10
-    direction_light.shadow.camera.bottom = -20
+    // // ライト
+    // const direction_light = new THREE.DirectionalLight( setting.light_color, setting.light_intensity );
+    // direction_light.position.set( setting.light_x, setting.light_y, setting.light_z );
+    // main_scene.add( direction_light );
+    // direction_light.castShadow = true;
+    // direction_light.shadow.camera.left = -20
+    // direction_light.shadow.camera.right = 10
+    // direction_light.shadow.camera.top = 10
+    // direction_light.shadow.camera.bottom = -20
 
-    // ライトヘルパー
-    const dlightHelper = new THREE.DirectionalLightHelper( direction_light, 25 );
-    main_scene.add( dlightHelper );
+    // // ライトヘルパー
+    // const dlightHelper = new THREE.DirectionalLightHelper( direction_light, 25 );
+    // main_scene.add( dlightHelper );
+    //
+    // // カメラヘルパー
+    // const dlightCameraHelper = new THREE.CameraHelper( direction_light.shadow.camera );
+    // main_scene.add( dlightCameraHelper );
 
-    // カメラヘルパー
-    const dlightCameraHelper = new THREE.CameraHelper( direction_light.shadow.camera );
-    main_scene.add( dlightCameraHelper );
+    const spotLight = new THREE.SpotLight(0xFFFFFF, 10, 150, Math.PI/5, 0, 0.5);
+    main_scene.add( spotLight );
+    spotLight.position.set( setting.light_x, setting.light_y, setting.light_z  );
+    spotLight.castShadow = true;
+
+    const spotLightHelper = new THREE.SpotLightHelper( spotLight );
+    main_scene.add( spotLightHelper );
 
 }
 
@@ -161,12 +169,13 @@ function setupMainScene(){
 
     const plane_geometry = new THREE.PlaneGeometry(setting.box_w*5, setting.box_h*5);
     const plane_material = new THREE.MeshStandardMaterial( {
-        color: 0xffff00,
+        color: 0xFFFFFF,
         side: THREE.DoubleSide // 両面表示
     } );
     plane_mesh = new THREE.Mesh( plane_geometry, plane_material );
     main_scene.add( plane_mesh );
     plane_mesh.receiveShadow = true;
+    plane_mesh.rotation.x = -0.5 * Math.PI
 
     const sphere_geometry = new THREE.SphereGeometry( 10, setting.sphere_segments, setting.sphere_segments);
     sphere_material = new THREE.MeshLambertMaterial( {
